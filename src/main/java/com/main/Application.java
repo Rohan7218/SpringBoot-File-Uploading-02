3267a3a3aae5228e6169574c4c2c09ff3ce8f3c8
Rohan7218 <sontakkerohan751@gmail.com>
2025-04-07 15:11:07 +0530

    second commit

--- src/main/java/com/main/Application.java
+++ src/main/java/com/main/Application.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
+public class Application 
+{
+	int x=10;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
