5e8b72e3fa1c3c03dc56f56916e1c9120a2769cc
Rohan7218 <sontakkerohan751@gmail.com>
2025-04-07 15:14:27 +0530

    third commit

--- src/main/java/com/main/Application.java
+++ src/main/java/com/main/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application 
 {
-	int x=10;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
